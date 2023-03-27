b89addc0d53045285b4afc0187eb9924cbb04696
chinet-developers <chinet.app@aol.com>
2023-03-27 18:49:18 +0200

    v1.5.2-fix-1

--- src/currency_core/blockchain_storage.cpp
+++ src/currency_core/blockchain_storage.cpp
@@ -476,48 +476,12 @@ bool blockchain_storage::set_lost_tx_unmixable_for_height(uint64_t height)
 //------------------------------------------------------------------
 bool blockchain_storage::set_lost_tx_unmixable()
 {  
-#ifndef TESTNET
-  if (m_db_blocks.size() > 75738)
-  {
-     crypto::hash tx_id_1 = epee::string_tools::parse_tpod_from_hex_string<crypto::hash>("c2a2229d614e7c026433efbcfdbd0be1f68d9b419220336df3e2c209f5d57314");
-     crypto::hash tx_id_2 = epee::string_tools::parse_tpod_from_hex_string<crypto::hash>("647f936c6ffbd136f5c95d9a90ad554bdb4c01541c6eb5755ad40b984d80da67");
- 
-     auto tx_ptr_1 = m_db_transactions.find(tx_id_1);
-     CHECK_AND_ASSERT_MES(tx_ptr_1, false, "Internal error: filed to find lost tx");
-     transaction_chain_entry tx1_local_entry(*tx_ptr_1);
-     for (size_t i = 0; i != tx1_local_entry.m_spent_flags.size(); i++)
-     {
-       tx1_local_entry.m_spent_flags[i] = true;
-     }
-     m_db_transactions.set(tx_id_1, tx1_local_entry);
-
-     auto tx_ptr_2 = m_db_transactions.find(tx_id_2);
-     transaction_chain_entry tx2_local_entry(*tx_ptr_2);
-     CHECK_AND_ASSERT_MES(tx_ptr_1, false, "Internal error: filed to find lost tx");
-     for (size_t i = 0; i != tx2_local_entry.m_spent_flags.size(); i++)
-     {
-       tx2_local_entry.m_spent_flags[i] = true;
-     }
-     m_db_transactions.set(tx_id_2, tx2_local_entry);
-  }
-#endif
   return true;
 }
 //------------------------------------------------------------------
 void  blockchain_storage::patch_out_if_needed(txout_to_key& out, const crypto::hash& tx_id, uint64_t n) const 
 {
-#ifndef TESTNET
-  static crypto::hash tx_id_1 = epee::string_tools::parse_tpod_from_hex_string<crypto::hash>("c2a2229d614e7c026433efbcfdbd0be1f68d9b419220336df3e2c209f5d57314");
-  static crypto::hash tx_id_2 = epee::string_tools::parse_tpod_from_hex_string<crypto::hash>("647f936c6ffbd136f5c95d9a90ad554bdb4c01541c6eb5755ad40b984d80da67");
-
-  if (tx_id == tx_id_1 && n == 12)
-  {
-    out.mix_attr = CURRENCY_TO_KEY_OUT_FORCED_NO_MIX;
-  }else if(tx_id == tx_id_2 && n == 5)
-  {
-    out.mix_attr = CURRENCY_TO_KEY_OUT_FORCED_NO_MIX;
-  }
-#endif
+  out.mix_attr = CURRENCY_TO_KEY_OUT_FORCED_NO_MIX
 }
 //------------------------------------------------------------------
 void blockchain_storage::store_db_solo_options_values()
